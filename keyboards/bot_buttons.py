8f6dbb6277f75eb0fdec0afa98243669b8905e61
DaniilSh23 <danyasevas11@gmail.com>
2023-04-19 22:48:39 +0300

    изменил название кнопки наши домены

--- keyboards/bot_buttons.py
+++ keyboards/bot_buttons.py
@@ -110,7 +110,7 @@ BUTTONS_DCT = {
         callback_data='short_link t9y.me'
     ),
     'custom_domain': InlineKeyboardButton(
-        text=f'🔹наш домен 👍',
+        text=f'🔹Наши домены 🏰',
         callback_data='short_link custom_domain'
     ),
 
